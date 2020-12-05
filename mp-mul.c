0517f7bb87e96b4e244f4ca9d067b6ef49c35109
Alexei A. Smekalkine <ikle@ikle.ru>
2020-12-05 19:55:20 +0300

    mp-mul: do not access y if ylen = 0, remove extra variable

--- mp-mul.c
+++ mp-mul.c
@@ -11,20 +11,18 @@
 mp_word mp_mul (mp_word *r, const mp_word *x, size_t xlen,
 			    const mp_word *y, size_t ylen)
 {
-	mp_word c, a = y[0];
+	mp_word c;
 	size_t i;
 
 	if (ylen == 0)
 		return 0;
 
-	c = mp_mul_1 (r, x, xlen, a);
+	c = mp_mul_1 (r, x, xlen, y[0]);
 
 	for (i = 1; i < ylen; ++i) {
 		r++[xlen] = c;
 
-		a = y[i];
-
-		c = mp_addmul_1 (r, x, xlen, a);
+		c = mp_addmul_1 (r, x, xlen, y[i]);
 	}
 
 	return c;
