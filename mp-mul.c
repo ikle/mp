e6d29c52e8dabe9ae8b23d1c371149fbbe5b43b1
Alexei A. Smekalkine <ikle@ikle.ru>
2020-12-15 00:07:08 +0300

    mp-mul: fix input carry propagation

--- mp-mul.c
+++ mp-mul.c
@@ -37,9 +37,16 @@ char mp_addmul_sb (digit_t *r, const digit_t *x, size_t xlen,
 	size_t i;
 	digit_t rh;
 
-	for (i = 0; i < ylen; ++i) {
-		rh = mp_addmul_1 (r + i, x, xlen, y[i], c);
-		c = mp_add_1 (r + xlen + i, r + xlen + i, ylen - i, rh);
+	/*
+	 * 1. mp_addmul_1 cannot oveflow: FF * F + FF + 1 = FF1
+	 * 2. mp_add_1 can overflow only once
+	 */
+	rh = mp_addmul_1 (r + 0, x, xlen, y[0], c);
+	c  = mp_add_1 (r + xlen + 0, r + xlen + 0, ylen - 0, rh);
+
+	for (i = 1; i < ylen; ++i) {
+		rh = mp_addmul_1 (r + i, x, xlen, y[i], 0);
+		c |= mp_add_1 (r + xlen + i, r + xlen + i, ylen - i, rh);
 	}
 
 	return c;
