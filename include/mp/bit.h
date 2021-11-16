8940a2c672b432bee43099b7b9620492468f5f3b
Alexei A. Smekalkine <ikle@ikle.ru>
2021-11-16 15:47:48 +0300

    bit: add extra pointers to descriptions

--- include/mp/bit.h
+++ include/mp/bit.h
@@ -16,6 +16,8 @@
 /*
  * The function mp_join joins set of bits (x, len) and (y, len), and stores
  * result into (r, len). In other words, it computes the bitwise OR.
+ *
+ * See also: Bounded lattice (algebra), Boolean algebra
  */
 static inline
 void mp_join (digit_t *r, const digit_t *x, const digit_t *y, size_t len)
@@ -29,6 +31,8 @@ void mp_join (digit_t *r, const digit_t *x, const digit_t *y, size_t len)
 /*
  * The function mp_meet meets set of bits (x, len) and (y, len), and stores
  * result into (r, len). In other words, it computes the bitwise AND.
+ *
+ * See also: Bounded lattice (algebra), Boolean algebra
  */
 static inline
 void mp_meet (digit_t *r, const digit_t *x, const digit_t *y, size_t len)
@@ -42,6 +46,8 @@ void mp_meet (digit_t *r, const digit_t *x, const digit_t *y, size_t len)
 /*
  * The function mp_comp complements set of bits (x, len), and stores result
  * into (r, len). In other words, it computes the bitwise NOT (inversion).
+ *
+ * See also: Complemented lattice (algebra), Boolean algebra
  */
 static inline void mp_comp (digit_t *r, const digit_t *x, size_t len)
 {
@@ -55,6 +61,8 @@ static inline void mp_comp (digit_t *r, const digit_t *x, size_t len)
  * The function mp_impl calculates the implication of set of bits (x, len)
  * and (y, len) -- Y implies X, and stores result into (r, len). In other
  * words, it computes the bitwise OR-NOT: R = ~X | Y.
+ *
+ * See also: Heyting algebra, Boolean algebra
  */
 static inline
 void mp_impl (digit_t *r, const digit_t *x, const digit_t *y, size_t len)
@@ -69,6 +77,8 @@ void mp_impl (digit_t *r, const digit_t *x, const digit_t *y, size_t len)
  * The function mp_diff calculates the difference of set of bits (x, len)
  * and (y, len), and stores result into (r, len). In other words, it
  * computes the bitwise AND-NOT: R = X & ~Y.
+ *
+ * See also: Set theory, Boolean algebra
  */
 static inline
 void mp_diff (digit_t *r, const digit_t *x, const digit_t *y, size_t len)
