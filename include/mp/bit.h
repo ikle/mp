bb070db54c8d079c6a77a5011b6caadcd1fd3e61
Alexei A. Smekalkine <ikle@ikle.ru>
2021-11-16 15:04:23 +0300

    bit: use upper case for boolean operators

--- include/mp/bit.h
+++ include/mp/bit.h
@@ -15,7 +15,7 @@
 
 /*
  * The function mp_join joins set of bits (x, len) and (y, len), and stores
- * result into (r, len). In other words, it computes the bitwise or.
+ * result into (r, len). In other words, it computes the bitwise OR.
  */
 static inline
 void mp_join (digit_t *r, const digit_t *x, const digit_t *y, size_t len)
@@ -28,7 +28,7 @@ void mp_join (digit_t *r, const digit_t *x, const digit_t *y, size_t len)
 
 /*
  * The function mp_meet meets set of bits (x, len) and (y, len), and stores
- * result into (r, len). In other words, it computes the bitwise and.
+ * result into (r, len). In other words, it computes the bitwise AND.
  */
 static inline
 void mp_meet (digit_t *r, const digit_t *x, const digit_t *y, size_t len)
@@ -41,7 +41,7 @@ void mp_meet (digit_t *r, const digit_t *x, const digit_t *y, size_t len)
 
 /*
  * The function mp_comp complements set of bits (x, len), and stores result
- * into (r, len). In other words, it computes the bitwise inversion.
+ * into (r, len). In other words, it computes the bitwise NOT (inversion).
  */
 static inline void mp_comp (digit_t *r, const digit_t *x, size_t len)
 {
@@ -54,7 +54,7 @@ static inline void mp_comp (digit_t *r, const digit_t *x, size_t len)
 /*
  * The function mp_impl calculates the back implication of set of bits
  * (x, len) and (y, len) -- Y implies X, and stores result into (r, len).
- * In other words, it computes the bitwise or-not: R = X | ~Y.
+ * In other words, it computes the bitwise OR-NOT: R = X | ~Y.
  */
 static inline
 void mp_impl (digit_t *r, const digit_t *x, const digit_t *y, size_t len)
@@ -68,7 +68,7 @@ void mp_impl (digit_t *r, const digit_t *x, const digit_t *y, size_t len)
 /*
  * The function mp_diff calculates the difference of set of bits (x, len)
  * and (y, len), and stores result into (r, len). In other words, it
- * computes the bitwise and-not: R = X & ~Y.
+ * computes the bitwise AND-NOT: R = X & ~Y.
  */
 static inline
 void mp_diff (digit_t *r, const digit_t *x, const digit_t *y, size_t len)
