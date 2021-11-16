673fbb3f33510714f3daaa0fad5d9f0c34a181c6
Alexei A. Smekalkine <ikle@ikle.ru>
2021-11-16 14:23:31 +0300

    bit: clean up descriptions

--- include/mp/bit.h
+++ include/mp/bit.h
@@ -14,8 +14,8 @@
 #include <mp/types.h>
 
 /*
- * The function mp_join performs the lattice join operation, or,
- * in other words, computes the bitwise or.
+ * The function mp_join joins set of bits (x, len) and (y, len), and stores
+ * result into (r, len). In other words, it computes the bitwise or.
  */
 static inline
 void mp_join (digit_t *r, const digit_t *x, const digit_t *y, size_t len)
@@ -27,8 +27,8 @@ void mp_join (digit_t *r, const digit_t *x, const digit_t *y, size_t len)
 }
 
 /*
- * The function mp_meet performs the lattice meet operation, or,
- * in other words, computes the bitwise and.
+ * The function mp_meet meets set of bits (x, len) and (y, len), and stores
+ * result into (r, len). In other words, it computes the bitwise and.
  */
 static inline
 void mp_meet (digit_t *r, const digit_t *x, const digit_t *y, size_t len)
@@ -40,8 +40,8 @@ void mp_meet (digit_t *r, const digit_t *x, const digit_t *y, size_t len)
 }
 
 /*
- * The function mp_comp performs the lattice complement operation, or,
- * in other words, computes the bitwise inversion.
+ * The function mp_comp complements set of bits (x, len), and stores result
+ * into (r, len). In other words, it computes the bitwise inversion.
  */
 static inline void mp_comp (digit_t *r, const digit_t *x, size_t len)
 {
