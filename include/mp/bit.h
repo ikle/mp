3a57832745ec8d3161d435e823b49ac0760628b2
Alexei A. Smekalkine <ikle@ikle.ru>
2021-11-19 22:21:13 +0300

    mp-bit: use code generation macroses, simplify

--- include/mp/bit.h
+++ include/mp/bit.h
@@ -13,20 +13,42 @@
 
 #include <mp/types.h>
 
+#define MP_BITWISE_ONE(name, op)					\
+static inline digit_t mp_digit_##name (digit_t x)			\
+{									\
+	return op;							\
+}									\
+									\
+static inline void mp_##name (digit_t *r, const digit_t *x, size_t len)	\
+{									\
+	size_t i;							\
+									\
+	for (i = 0; i < len; ++i)					\
+		r[i] = mp_digit_##name (x[i]);				\
+}
+
+#define MP_BITWISE_TWO(name, op)					\
+static inline digit_t mp_digit_##name (digit_t x, digit_t y)		\
+{									\
+	return op;							\
+}									\
+									\
+static inline void mp_##name (digit_t *r, const digit_t *x,		\
+			      const digit_t *y, size_t len)		\
+{									\
+	size_t i;							\
+									\
+	for (i = 0; i < len; ++i)					\
+		r[i] = mp_digit_##name (x[i], y[i]);			\
+}
+
 /*
  * The function mp_join joins set of bits (x, len) and (y, len), and stores
  * result into (r, len). In other words, it computes the bitwise OR.
  *
  * See also: Bounded lattice (algebra), Boolean algebra
  */
-static inline
-void mp_join (digit_t *r, const digit_t *x, const digit_t *y, size_t len)
-{
-	size_t i;
-
-	for (i = 0; i < len; ++i)
-		r[i] = x[i] | y[i];
-}
+MP_BITWISE_TWO (join, x | y)
 
 /*
  * The function mp_meet meets set of bits (x, len) and (y, len), and stores
@@ -34,14 +56,7 @@ void mp_join (digit_t *r, const digit_t *x, const digit_t *y, size_t len)
  *
  * See also: Bounded lattice (algebra), Boolean algebra
  */
-static inline
-void mp_meet (digit_t *r, const digit_t *x, const digit_t *y, size_t len)
-{
-	size_t i;
-
-	for (i = 0; i < len; ++i)
-		r[i] = x[i] & y[i];
-}
+MP_BITWISE_TWO (meet, x & y)
 
 /*
  * The function mp_comp complements set of bits (x, len), and stores result
@@ -49,13 +64,7 @@ void mp_meet (digit_t *r, const digit_t *x, const digit_t *y, size_t len)
  *
  * See also: Complemented lattice (algebra), Boolean algebra
  */
-static inline void mp_comp (digit_t *r, const digit_t *x, size_t len)
-{
-	size_t i;
-
-	for (i = 0; i < len; ++i)
-		r[i] = ~x[i];
-}
+MP_BITWISE_ONE (comp, ~x)
 
 /*
  * The function mp_impl calculates the implication of set of bits (x, len)
@@ -64,14 +73,7 @@ static inline void mp_comp (digit_t *r, const digit_t *x, size_t len)
  *
  * See also: Heyting algebra, Boolean algebra
  */
-static inline
-void mp_impl (digit_t *r, const digit_t *x, const digit_t *y, size_t len)
-{
-	size_t i;
-
-	for (i = 0; i < len; ++i)
-		r[i] = ~x[i] | y[i];
-}
+MP_BITWISE_TWO (impl, ~x | y)
 
 /*
  * The function mp_diff calculates the difference of set of bits (x, len)
@@ -80,14 +82,7 @@ void mp_impl (digit_t *r, const digit_t *x, const digit_t *y, size_t len)
  *
  * See also: Set theory, Boolean algebra
  */
-static inline
-void mp_diff (digit_t *r, const digit_t *x, const digit_t *y, size_t len)
-{
-	size_t i;
-
-	for (i = 0; i < len; ++i)
-		r[i] = x[i] & ~y[i];
-}
+MP_BITWISE_TWO (diff, x & ~y)
 
 /*
  * The function mp_xor calculates the symmetric difference of set of bits
@@ -96,13 +91,6 @@ void mp_diff (digit_t *r, const digit_t *x, const digit_t *y, size_t len)
  *
  * See also: Boolean ring
  */
-static inline
-void mp_xor (digit_t *r, const digit_t *x, const digit_t *y, size_t len)
-{
-	size_t i;
-
-	for (i = 0; i < len; ++i)
-		r[i] = x[i] ^ y[i];
-}
+MP_BITWISE_TWO (xor, x ^ y)
 
 #endif  /* MP_BIT_H */
