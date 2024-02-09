8f8898e1c5d9380453dc74160cbc4a352f527ddf
Alexei A. Smekalkine <ikle@ikle.ru>
2024-02-09 06:21:19 +0300

    mp-mul-1-generic: separate submul variant

---
+++ mp-submul-1-generic.c
@@ -0,0 +1,23 @@
+/*
+ * MP Core Multiplication: Substract from Result Variant, Generic Implemention
+ *
+ * Copyright (c) 2014-2024 Alexei A. Smekalkine <ikle@ikle.ru>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <mp/digit.h>
+#include <mp/mul.h>
+
+digit_t mp_submul_1 (digit_t *r, const digit_t *x, size_t len, digit_t y, int c)
+{
+	size_t i;
+	digit_t r1 = c, r0;
+
+	for (i = 0; i < len; ++i) {
+		mp_digit_fma (&r1, &r0, x[i], y, r1);
+		r1 += mp_digit_sub (r + i, r[i], r0);
+	}
+
+	return r1;
+}
