704f059be0b560143e7a975b7a353c802dcf47e1
Alexei A. Smekalkine <ikle@ikle.ru>
2024-02-09 06:20:58 +0300

    mp-mul-1-generic: separate addmul variant

--- mp-mul-1-generic.c
+++ mp-mul-1-generic.c
@@ -20,19 +20,6 @@ digit_t mp_mul_1 (digit_t *r, const digit_t *x, size_t len, digit_t y)
 	return c;
 }
 
-digit_t mp_addmul_1 (digit_t *r, const digit_t *x, size_t len, digit_t y, int c)
-{
-	size_t i;
-	digit_t r1 = c, r0;
-
-	for (i = 0; i < len; ++i) {
-		mp_digit_fma (&r1, &r0, x[i], y, r1);
-		r1 += mp_digit_add (r + i, r[i], r0);
-	}
-
-	return r1;
-}
-
 digit_t mp_submul_1 (digit_t *r, const digit_t *x, size_t len, digit_t y, int c)
 {
 	size_t i;
