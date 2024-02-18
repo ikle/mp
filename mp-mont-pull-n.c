08ca9155d2007e7bfe2f005a5b63800b96bd8ee8
Alexei A. Smekalkine <ikle@ikle.ru>
2024-02-18 03:07:54 +0300

    mp-mont-mul-n: separate reduction helper again

--- mp-mont-pull-n.c
+++ mp-mont-pull-n.c
@@ -1,5 +1,5 @@
 /*
- * MP Core Modular Arithmetics: Montgomery Multiplication
+ * MP Core Modular Arithmetics: Montgomery Reduction
  *
  * Copyright (c) 2014-2024 Alexei A. Smekalkine <ikle@ikle.ru>
  *
@@ -9,20 +9,20 @@
 #include <mp/mont-mul.h>
 #include <mp/mul.h>
 #include <mp/shift.h>
+#include <mp/unit.h>
 
-void mp_mont_mul_n (digit_t *r, const digit_t *x, const digit_t *y,
-		    const digit_t *m, size_t len, digit_t mu)
+void mp_mont_pull_n (digit_t *r, const digit_t *x,
+		     const digit_t *m, size_t len, digit_t mu)
 {
 	digit_t rc;
 	size_t i;
 
-	rc  = mp_mul_1 (r, x, len, y[0]);
-	rc += mp_addmul_1 (r, m, len, mu * r[0], 0);
+	mp_copy (r, x, len);
+	rc = mp_addmul_1 (r, m, len, mu * r[0], 0);
 	mp_rshift_word (r, r, len, rc);
 
 	for (i = 1; i < len; ++i) {
-		rc  = mp_addmul_1 (r, x, len, y[i], 0);
-		rc += mp_addmul_1 (r, m, len, mu * r[0], 0);
+		rc = mp_addmul_1 (r, m, len, mu * r[0], 0);
 		mp_rshift_word (r, r, len, rc);
 	}
 
