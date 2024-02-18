b23279e545dd7c6d267b14aa58a3eb5c6993acd7
Alexei A. Smekalkine <ikle@ikle.ru>
2024-02-18 03:01:21 +0300

    mp-mont-mul-n: make helper shift right for one digit public

--- include/mp/shift.h
+++ include/mp/shift.h
@@ -9,6 +9,8 @@
 #ifndef MP_SHIFT_H
 #define MP_SHIFT_H  1
 
+#include <string.h>
+
 #include <mp/types.h>
 
 /*
@@ -19,8 +21,18 @@
  * Function mp_rshift divides (x, len) by 2^n, stores result bitwise ored
  * with input carry at high digit into (r, len), and returns the remainder
  * value (shifted-out bits). The n must be less than MP_DIGIT_BITS.
+ *
+ * Function mp_rshift_word shifts one digit right, stores result into
+ * (r, len) with input carry at high digit.
  */
 digit_t mp_lshift (digit_t *r, const digit_t *x, size_t len, digit_t c, int n);
 digit_t mp_rshift (digit_t *r, const digit_t *x, size_t len, digit_t c, int n);
+
+static inline
+void mp_rshift_word (digit_t *r, const digit_t *x, size_t len, digit_t c)
+{
+	memmove (r, x + 1, (len - 1) * sizeof (x[0]));
+	r[len - 1] = c;
+}
 
 #endif  /* MP_SHIFT_H */
--- mp-mont-mul-n.c
+++ mp-mont-mul-n.c
@@ -8,14 +8,8 @@
 
 #include <mp/mont-mul.h>
 #include <mp/mul.h>
+#include <mp/shift.h>
 #include <mp/unit.h>
-
-static inline
-void mp_rshift_word (digit_t *r, const digit_t *x, size_t len, digit_t c)
-{
-	memmove (r, x + 1, (len - 1) * sizeof (x[0]));
-	r[len - 1] = c;
-}
 
 void mp_mont_mul_n (digit_t *r, const digit_t *x, const digit_t *y,
 		    const digit_t *m, size_t len, digit_t mu)
