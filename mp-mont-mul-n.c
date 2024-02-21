93c7b0fd6609e2595fddce50a286b7ded4f58c5b
Alexei A. Smekalkine <ikle@ikle.ru>
2024-02-21 05:19:42 +0300

    mp-mont-mul-n: fix calculation, check for overflow

--- mp-mont-mul-n.c
+++ mp-mont-mul-n.c
@@ -6,6 +6,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <mp/digit.h>
 #include <mp/mont-mul.h>
 #include <mp/mul.h>
 #include <mp/shift.h>
@@ -14,18 +15,20 @@ void mp_mont_mul_n (digit_t *r, const digit_t *x, const digit_t *y,
 		    const digit_t *m, size_t len, digit_t mu)
 {
 	digit_t rc;
+	char c, nc;
 	size_t i;
 
 	rc  = mp_mul_1 (r, x, len, y[0]);
-	rc += mp_addmul_1 (r, m, len, mu * r[0], 0);
+	c = mp_digit_add (&rc, rc, mp_addmul_1 (r, m, len, mu * r[0], 0));
 	mp_rshift_word (r, r, len, rc);
 
 	for (i = 1; i < len; ++i) {
 		rc  = mp_addmul_1 (r, x, len, y[i], 0);
-		rc += mp_addmul_1 (r, m, len, mu * r[0], 0);
-		mp_rshift_word (r, r, len, rc);
+		nc = mp_digit_add (&rc, rc, mp_addmul_1 (r, m, len, mu * r[0], 0));
+		mp_rshift_word (r, r, len, rc + c);
+		c = nc;
 	}
 
-	if (mp_cmp_n (r, m, len) >= 0)
+	if (c != 0 || mp_cmp_n (r, m, len) >= 0)
 		mp_sub_n (r, r, m, len, 0);
 }
