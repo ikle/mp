968f77f7a14fccc63e43fc07cb4f6c5b5681a63f
Alexei A. Smekalkine <ikle@ikle.ru>
2024-02-23 04:49:32 +0300

    mp-mont-ro: add alternative implementation

--- mp-mont-ro.c
+++ mp-mont-ro.c
@@ -14,10 +14,20 @@
 
 void mp_mont_ro (digit_t *r, const digit_t *m, size_t len)
 {
+#if 1
 	const size_t n = len * 2;
 	digit_t R2[n + 1];
 
 	mp_zero (R2, n); R2[n] = 1;  /* R^2, where R = B^len */
 
 	mp_mod (r, R2, n + 1, m, len);
+#else
+	size_t i, j;
+
+	mp_neg (r, m, len);
+
+	for (i = 0; i < len; ++i)
+		for (j = 0; j < MP_DIGIT_BITS; ++j)
+			mp_mod_add_n (r, r, r, m, len);
+#endif
 }
