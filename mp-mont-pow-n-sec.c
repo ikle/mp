41e85feb3dd004004f24bab15943c294c5671be8
Alexei A. Smekalkine <ikle@ikle.ru>
2024-02-23 22:16:12 +0300

    mp-mont-pow-n: separate secure variant

--- mp-mont-pow-n-sec.c
+++ mp-mont-pow-n-sec.c
@@ -1,5 +1,5 @@
 /*
- * MP Core Modular Exponentiation in Montgomery Form
+ * MP Core Modular Exponentiation in Montgomery Form, Secure Variant
  *
  * Copyright (c) 2014-2024 Alexei A. Smekalkine <ikle@ikle.ru>
  *
@@ -9,8 +9,8 @@
 #include <mp/mont-mul.h>
 #include <mp/unit.h>
 
-void mp_mont_pow_n (digit_t *r, const digit_t *x, const digit_t *y,
-		    const digit_t *m, size_t len, digit_t mu)
+void mp_mont_pow_n_sec (digit_t *r, const digit_t *x, const digit_t *y,
+			const digit_t *m, size_t len, digit_t mu)
 {
 	size_t i, j;
 	digit_t d, X[len], t[len];
@@ -19,10 +19,8 @@ void mp_mont_pow_n (digit_t *r, const digit_t *x, const digit_t *y,
 
 	for (i = 0; i < len; ++i)
 		for (j = 0, d = y[i]; j < MP_DIGIT_BITS; ++j, d >>= 1) {
-			if ((d & 1) != 0) {
-				mp_mont_mul_n (t, r, X, m, len, mu);
-				mp_copy (r, t, len);
-			}
+			mp_mont_mul_n (t, r, X, m, len, mu);
+			mp_copy ((d & 1) != 0 ? r : t, t, len);
 
 			mp_mont_mul_n (t, X, X, m, len, mu);
 			mp_copy (X, t, len);
